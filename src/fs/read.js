13b4eab43c8d439224a7c1d06340a0c909750260
Vladimir Tugutov <tugutov@gmail.com>
2025-11-09 01:25:11 -0300

    feat: add fs/read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,19 +1,19 @@
-import { readdir } from 'fs/promises';
+import { readFile } from 'fs/promises';
 import { dirname, join } from 'path';
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const list = async () => {
-  const folderPath = join(__dirname, 'files');
+const read = async () => {
+  const filePath = join(__dirname, 'files', 'fileToRead.txt');
 
   try {
-    const files = await readdir(folderPath);
-    console.log(files);
+    const data = await readFile(filePath, { encoding: 'utf-8' });
+    console.log(data);
   } catch (err) {
     throw new Error('FS operation failed');
   }
 };
 
-await list();
+await read();
